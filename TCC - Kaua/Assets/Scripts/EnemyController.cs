7390bb92caf38697b417a38cdc32b0bc885b7fde
MiguelPequini <166048444+MiguelPequini@users.noreply.github.com>
2025-10-18 09:30:16 -0300

    TCC

---
+++ TCC - Kaua/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float MoveSpeed; 
+    private Rigidbody2D rb;
+
+    public Transform Enemy;
+    public Transform Target;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        ChasePlayer();
+    }
+
+    void ChasePlayer()
+    {
+        float MoveX, MoveY;
+        
+        if (Enemy.position.x < Target.position.x)
+        {
+            MoveX = 1;
+        }
+        else { MoveX = -1; }
+
+        if (Enemy.position.y < Target.position.y)
+        {
+            MoveY = 1;
+        }
+        else { MoveY = -1; }
+
+        Vector2 Direction = new Vector2(MoveX, MoveY);
+        rb.velocity = MoveSpeed * Direction.normalized;
+
+    }
+}
---
+++ TCC - Kaua/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MoveSpeed;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float MoveX, MoveY;
+
+        MoveX = Input.GetAxisRaw("Horizontal");
+        MoveY = Input.GetAxisRaw("Vertical");
+
+        Vector2 Direction = new Vector2(MoveX, MoveY);
+        rb.velocity = MoveSpeed * Direction.normalized;
+    }
+}
